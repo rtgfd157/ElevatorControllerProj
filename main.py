c50966974d1cea2262dd61b370b2d19f13f8e35b
idan z <idanz1234u@gmail.com>
2020-11-20 14:59:01 +0200

    added instance of floors in ElevatorController

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 
 from elevatorClass import Elevator
+from floorsClass import FloorsClass
 
 class ElevatorController:
     def __init__(self,in_currentFloor, in_number_of_floors):
 
-        elevator_instance =  Elevator(in_currentFloor, in_number_of_floors) 
+        self.elevator_instance =  Elevator(in_currentFloor, in_number_of_floors) 
 
-
+        elevatorPlace = self.elevator_instance.currentFloor # elevator current place
+        self.floors_instance = FloorsClass(elevatorPlace , in_number_of_floors)  
 
     def startprogram(self):
-        print("Hello World") ;
+        print("Hello World") 
 
 if __name__ == "__main__":
     current_floor = 0
